65816d7294ca816acd0f4b94e5507f0adbf4394d
Юрий Елисеев <yuriiyelisieiev@Urijs-MacBook-Pro.local>
2020-03-26 14:45:33 +0200

    Refactored and fixed BUG with progress bar

--- modules.py
+++ modules.py
@@ -11,14 +11,13 @@ from pytorch_lightning.callbacks import ModelCheckpoint
 from datasets import INbreastDataset
 
 
-class InBreastSystem(pl.LightningModule):
+class INBreastClassification(pl.LightningModule):
     def __init__(self, config):
-        super(InBreastSystem, self).__init__()
+        super(INBreastClassification, self).__init__()
         self.config = config
         self.n_class = len(config['data']['groups'])
         self.criteria = nn.CrossEntropyLoss()
         self.__load_model()
-        self.__load_loaders()
 
     def forward(self, x):
         return self.model(x)
@@ -28,16 +27,15 @@ class InBreastSystem(pl.LightningModule):
         y_pred = self.forward(x)
         loss = self.criteria(y_pred, y)
         loss = loss.unsqueeze(dim=-1)
-        logs = {'loss': loss}
-        return {'loss': loss, 'logs': logs}
+        return {'loss': loss, 'log': {'loss': loss}}
 
-    def validation_step(self, batch, batch_idx):
-        x, y = batch
-        y_pred = self.forward(x)
-        val_loss = self.criteria(y_pred, y)
-        val_loss = val_loss.unsqueeze(dim=-1)
-        logs = {'val_loss': val_loss}
-        return {'val_loss': val_loss, 'logs': logs}
+    # def validation_step(self, batch, batch_idx):
+    #     x, y = batch
+    #     y_pred = self.forward(x)
+    #     val_loss = self.criteria(y_pred, y)
+    #     val_loss = val_loss.unsqueeze(dim=-1)
+    #     self.logger.summary.scalar('loss', val_loss)
+    #     return {'val_loss': val_loss}
 
     def configure_optimizers(self):
         optimizer = Adam(self.model.parameters(), lr=0.001)
@@ -45,27 +43,6 @@ class InBreastSystem(pl.LightningModule):
         return [optimizer], [scheduler]
 
     def train_dataloader(self):
-        return self.train_loader
-
-    # @pl.data_loader
-    # def val_dataloader(self):
-    #     return self.val_dataloader
-
-    @staticmethod
-    def __module_mapping(module_name):
-        mapping = {}
-        for name, obj in inspect.getmembers(importlib.import_module(module_name), inspect.isclass):
-            mapping[name] = obj
-        return mapping
-
-    def __load_model(self):
-        mapping = self.__module_mapping('models')
-        if 'parameters' not in self.config['model']:
-            self.config['model']['parameters'] = {}
-        self.config['model']['parameters']['n_classes'] = self.n_class
-        self.model = mapping[self.config['model']['name']](**self.config['model']['parameters']).get_model()
-
-    def __load_loaders(self):
         transform = transforms.Compose([
             transforms.ToPILImage(),
             transforms.Resize((256, 256)),
@@ -74,5 +51,19 @@ class InBreastSystem(pl.LightningModule):
         root_dir = self.config["data"]["data_dir"]
         dataset = eval(self.config["data"]["data_set"])
         train_dataset = dataset(root_dir, partition="train", config=self.config["data"], transform=transform)
-        self.train_loader = DataLoader(train_dataset, batch_size=16, pin_memory=True, shuffle=True)
-        print(len(self.train_loader))
+        return DataLoader(train_dataset, batch_size=16, shuffle=True)
+    # TODO: Implement validation dataloader
+
+    def __load_model(self):
+        mapping = self.__module_mapping('models')
+        if 'parameters' not in self.config['model']:
+            self.config['model']['parameters'] = {}
+        self.config['model']['parameters']['n_classes'] = self.n_class
+        self.model = mapping[self.config['model']['name']](**self.config['model']['parameters']).get_model()
+
+    @staticmethod
+    def __module_mapping(module_name):
+        mapping = {}
+        for name, obj in inspect.getmembers(importlib.import_module(module_name), inspect.isclass):
+            mapping[name] = obj
+        return mapping
--- train.py
+++ train.py
@@ -1,10 +1,11 @@
 from pytorch_lightning import Trainer
-from modules import InBreastSystem
+from tqdm.auto import tqdm
+from modules import INBreastClassification
 import yaml
 
 if __name__ == '__main__':
     with open('config.yaml', 'r') as f:
         config = yaml.load(f, Loader=yaml.FullLoader)
-    model = InBreastSystem(config)
-    trainer = Trainer(max_nb_epochs=20)
+    model = INBreastClassification(config)
+    trainer = Trainer(max_epochs=20, gpus=3, progress_bar_refresh_rate=1)
     trainer.fit(model)
